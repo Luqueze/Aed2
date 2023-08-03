3649d52a815689fbeda0e9c4d2981f11893ab9ad
Lucas Caldeira <93887953+Luqueze@users.noreply.github.com>
2023-08-03 19:27:58 -0300

    Add files via upload

---
+++ exercicios_nivelamento/estacontido(1).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+bool estacontido(int vetor[], int num){
+    for(int i = 0; i < 10; i++){
+        if(vetor[i] == num){
+            return true;
+        }
+    }
+    return false;
+} 
