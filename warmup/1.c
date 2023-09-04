efce5a6c36ebeb29293b3e17c8d338f455a9b19c
Lucas <lucashaleluia@hotmail.com>
2023-09-04 13:56:47 -0300

    add file

---
+++ warmup/1.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+
+void combinador(char *str1, char *str2, char *str3){
+    int i = 0, j = 0, k = 0;
+    while(str1[i] != '\0' && str2[j] != '\0'){
+        str3[k++] = str1[i++];
+        str3[k++] = str2[j++];
+    }
+    while(str1[i] != '\0'){
+        str3[k++] = str1[i++];
+    }
+    while(str2[j] != '\0'){
+        str3[k++] = str2[j++];
+    }
+    str3[k] = '\0';
+}
+int main(){
+    char str1[100], str2[100], str3[200];
+    fgets(str1, 100, stdin);
+    fgets(str2, 100, stdin);
+
+
+    combinador(str1, str2, str3);
+    printf("%s\n", str3);
+    return 0;
+}
